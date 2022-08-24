7e32e0a9ec985e099e0b73a57b66d9f8d0346760
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-08-24 16:42:35 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,20 @@
+const TABLE_NAME = 'all-for-you';
+const config = {
+    PORT: process.env.PORT || 5000,
+    //DB_CONNECTION: `mongodb://localhost/${TABLE_NAME}`,
+    DB_CONNECTION: `mongodb+srv://dy707:Draze999@cluster0.ruqu4pf.mongodb.net/?retryWrites=true&w=majority`,
+    SECRET: 'badumts',
+    SALT: 10,
+    COOKIE_NAME: 'USER_SESSION',
+    CLOUDINARY_NAME: 'dulmusxxc',
+    CLOUDINARY_API_KEY: 359525114473425,
+    CLOUDINARY_API_SECRET: '3hra-Y3Je0Xl8O0vrVsewnhWXCI',
+    CLOUDINARY_STORAGE: 'pza5zln6'
+
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/'))
+}
+
+module.exports = config;
